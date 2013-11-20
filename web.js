6509daddbbe619609b28334aab370f21e4ede14f
Dan Kwon <dkwon86116@gmail.com>
2013-11-20 04:27:06 +0000

    Changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+fs.readFileSync(index.html)
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString);
 });
 
 var port = process.env.PORT || 5000;
