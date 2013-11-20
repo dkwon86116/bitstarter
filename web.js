ce3d7be6bc7bcf6591745f28384afb194478cfeb
Dan Kwon <dkwon86116@gmail.com>
2013-11-20 06:55:43 +0000

    should work!

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
-var index_buf = fs.readFileSync('index.html')
+var index_buf = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
   response.send(index_buf.toString());
